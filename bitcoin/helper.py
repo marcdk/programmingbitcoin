6c820588212f9f4aa844304ec0ea551a1ba4eded
Marc de Klerk <deklerkmc@gmail.com>
2020-07-21 08:36:11 +0200

    ex 11.3

--- bitcoin/helper.py
+++ bitcoin/helper.py
@@ -200,6 +200,17 @@ def merkle_parent_level(hashes):
 
     return parent_level
 
+def merkle_root(hashes):
+    '''Takes a list of binary hashes and returns the merkle root
+    '''
+
+    merkle_level = hashes
+
+    while len(merkle_level) > 1:
+        merkle_level = merkle_parent_level(merkle_level)
+
+    return merkle_level[0]
+
 class HelperTest(TestCase):
 
     def test_little_endian_to_int(self):
@@ -277,3 +288,23 @@ class HelperTest(TestCase):
         ]
         want_tx_hashes = [bytes.fromhex(x) for x in want_hex_hashes]
         self.assertEqual(merkle_parent_level(tx_hashes), want_tx_hashes)
+
+    def test_merkle_root(self):
+        hex_hashes = [
+            'c117ea8ec828342f4dfb0ad6bd140e03a50720ece40169ee38bdc15d9eb64cf5',
+            'c131474164b412e3406696da1ee20ab0fc9bf41c8f05fa8ceea7a08d672d7cc5',
+            'f391da6ecfeed1814efae39e7fcb3838ae0b02c02ae7d0a5848a66947c0727b0',
+            '3d238a92a94532b946c90e19c49351c763696cff3db400485b813aecb8a13181',
+            '10092f2633be5f3ce349bf9ddbde36caa3dd10dfa0ec8106bce23acbff637dae',
+            '7d37b3d54fa6a64869084bfd2e831309118b9e833610e6228adacdbd1b4ba161',
+            '8118a77e542892fe15ae3fc771a4abfd2f5d5d5997544c3487ac36b5c85170fc',
+            'dff6879848c2c9b62fe652720b8df5272093acfaa45a43cdb3696fe2466a3877',
+            'b825c0745f46ac58f7d3759e6dc535a1fec7820377f24d4c2c6ad2cc55c0cb59',
+            '95513952a04bd8992721e9b7e2937f1c04ba31e0469fbe615a78197f68f52b7c',
+            '2e6d722e5e4dbdf2447ddecc9f7dabb8e299bae921c99ad5b0184cd9eb8e5908',
+            'b13a750047bc0bdceb2473e5fe488c2596d7a7124b4e716fdd29b046ef99bbf0',
+        ]
+        tx_hashes = [bytes.fromhex(x) for x in hex_hashes]
+        want_hex_hash = 'acbcab8bcc1af95d8d563b77d24c3d19b18f1486383d75a5085c4e86c86beed6'
+        want_hash = bytes.fromhex(want_hex_hash)
+        self.assertEqual(merkle_root(tx_hashes), want_hash)
